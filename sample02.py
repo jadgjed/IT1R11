a1a884d1a684ca3d8389d5ec49112a5dc637f66d
jadgjed <dacoborjewel20@gmail.com>
2026-01-22 15:05:07 +0800

    Add basic arithmetic operations in sample02.py

---
+++ sample02.py
@@ -0,0 +1,14 @@
+num1 = 67
+num2 = 7
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print("BASIC ARITHMETIC OPERATION")
+print("--------------------------")
+print("Addition        :", addition)
+print("Subtraction     :", subtraction)
+print("Multiplication  :", multiplication)
+print("Division        :", division)
